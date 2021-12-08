acf1ce37550a730c363674485f6eda1817236062
herumes <91907013+herumes@users.noreply.github.com>
2021-12-08 02:56:13 -0800

    Update WebSocketClient.java

--- src/main/java/net/dv8tion/jda/internal/requests/WebSocketClient.java
+++ src/main/java/net/dv8tion/jda/internal/requests/WebSocketClient.java
@@ -696,8 +696,8 @@ public class WebSocketClient extends WebSocketAdapter implements WebSocketListen
         PresenceImpl presenceObj = (PresenceImpl) api.getPresence();
         DataObject connectionProperties = DataObject.empty()
             .put("$os", System.getProperty("os.name"))
-            .put("$browser", "Discord iOS")
-            .put("$device", "JDA")
+            .put("$browser", "Discord Android")
+            .put("$device", "Discord Android")
             .put("$referring_domain", "")
             .put("$referrer", "");
         DataObject payload = DataObject.empty()
